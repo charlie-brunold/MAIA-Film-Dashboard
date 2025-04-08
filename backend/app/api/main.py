340a4f0a2df218f65d72cf9853548176e7133e80
alicej06 <alicejiangt@gmail.com>
2025-04-07 20:32:06 -0700

    Update main.py

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
 import pandas as pd
-from preprocess import preprocess_input
+import joblib
+import os
 
 app = FastAPI()
-model = joblib.load("model.pkl")
+model_path = os.path.join(os.path.dirname(__file__), 'models/revenue_model.pkl')
+model = joblib.load(model_path)
 
 class MovieFeatures(BaseModel):
+    score: float
+    genre: str
+    crew: str
+    orig_lang: str
     budget: float
-    popularity: float
-    runtime: float
-    release_year: int
-    release_month: int
-    release_day: int
-    genres: list[str]
+    country: str
+    budget_std: float
 
 @app.post("/predict")
 def predict_revenue(movie: MovieFeatures):
-    input_df = pd.DataFrame([movie.dict()])
-    input_processed = preprocess_input(input_df)
-    prediction = model.predict(input_processed)[0]
+    input_data = pd.DataFrame([movie.dict()])
+    prediction = model.predict(input_data)[0]
     return {"predicted_revenue": round(prediction)}
